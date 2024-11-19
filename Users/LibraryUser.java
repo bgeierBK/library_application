c32e98bfb5f698a655d0b92a96ef200ba59b307d
bmgeier <geier.benjamin@gmail.com>
2024-11-18 19:33:01 -0500

    initial commit/create first classes

---
+++ Users/LibraryUser.java
@@ -0,0 +1,14 @@
+package Users;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class LibraryUser {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Integer passCode;
+
+
+
+}
